04520ce3cf499b5e598dd32305b363ebdb9e69ec
PauloGN <paulorsantos.gnt@outlook.com>
2023-10-18 19:25:57 -0700

    [96] Magic Damage

--- EternalSage/Assets/Scripts/Entity/EntityStats.cs
+++ EternalSage/Assets/Scripts/Entity/EntityStats.cs
@@ -12,11 +12,22 @@ public class EntityStats : MonoBehaviour
     public Stat maxHealth;
     public Stat armor;
     public Stat evasion;
+    public Stat magicalResistance;
     [Space]
     [Header("Offensive Stats")]
     public Stat damage;
     public Stat critChance;
     public Stat critPower;       //Default value 150%
+    [Header("Magic Stats")]
+    public Stat fireDamage;
+    public Stat iceDamage;
+    public Stat metalDamage;
+    public int intFactor = 3;   //multipplies the intelligence factor
+
+    //Bool controllers
+    public bool isIgnited;
+    public bool isChilled;
+    public bool isShocked;
 
     //internal controllers
     [SerializeField] private int currentHelth;
@@ -27,15 +38,44 @@ public class EntityStats : MonoBehaviour
         critPower.SetdefaultValue(150);
     }
 
+    public virtual void DoMagicalDamage(EntityStats _targetStats)
+    {
+        //calculate the total damage to be applied
+        int _fireDamage = fireDamage.GetValue();
+        int _iceDamage = iceDamage.GetValue();
+        int _metalDamage = metalDamage.GetValue();
+        int totalMagicalDmg = _fireDamage + _iceDamage + _metalDamage + intelligence.GetValue();
+
+        //Calculate the total resistence to deduct from the total magical damage
+        totalMagicalDmg = CheckTargetMagicalResistence(_targetStats, totalMagicalDmg);
+
+        //Apply damage on target
+        _targetStats.TakeDamage(totalMagicalDmg);
+    }
+
+
+    public void ApplyElements(bool _ignite, bool _chil, bool _shock)
+    {
+        if (isChilled || isChilled || isShocked)
+        {
+            return;
+        }
+
+        isShocked = _shock;
+        isIgnited = _ignite;
+        isChilled = _chil;
+    }
+
     public virtual void DoDamage(EntityStats _targetStats)
     {
-        //Check evaision 
+        //Check evasion 
         if (TargetCanAvoidAttack(_targetStats))
         {
             return;
         }
 
         int totalDamage = damage.GetValue() + strength.GetValue();
+
         //Check Critical damage chance and amount
         if (CanCrit())
         {
@@ -45,7 +85,8 @@ public class EntityStats : MonoBehaviour
 
         //Check Armor power and returns the damage to be aplied
         totalDamage = CheckTargetArmor(_targetStats, totalDamage);
-        _targetStats.TakeDamage(totalDamage);
+        //  _targetStats.TakeDamage(totalDamage);
+        DoMagicalDamage(_targetStats);
     }
 
 
@@ -68,6 +109,13 @@ public class EntityStats : MonoBehaviour
     }
 
     #region Stats Check and Mechanics
+    //Magic
+    private int CheckTargetMagicalResistence(EntityStats _targetStats, int totalMagicalDmg)
+    {
+        totalMagicalDmg -= _targetStats.magicalResistance.GetValue() + (_targetStats.intelligence.GetValue() * intFactor);
+        totalMagicalDmg = Mathf.Clamp(totalMagicalDmg, 0, int.MaxValue);
+        return totalMagicalDmg;
+    }
     private int CheckTargetArmor(EntityStats _targetStats, int totalDamage)
     {
         totalDamage -= _targetStats.armor.GetValue();
@@ -103,5 +151,4 @@ public class EntityStats : MonoBehaviour
         return Mathf.RoundToInt(critDamage);
     }
     #endregion
-
 }
