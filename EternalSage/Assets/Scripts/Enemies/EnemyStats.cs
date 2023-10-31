313f6018bd4a1128ca2a2cd0b5fbaa7baea89f94
PauloGN <paulorsantos.gnt@outlook.com>
2023-10-30 19:13:44 -0700

    [111] Enemy modifiers

--- EternalSage/Assets/Scripts/Enemies/EnemyStats.cs
+++ EternalSage/Assets/Scripts/Enemies/EnemyStats.cs
@@ -6,16 +6,54 @@ public class EnemyStats : EntityStats
 {
 
     Enemy enemy;
+    [Header("Level details")]
+    [SerializeField] private int level = 1;
+    [Range(0f, 1f)]
+    [SerializeField] private float percentageModifier = .4f;
 
     protected override void Start()
     {
+        //Modify enemy properties based on its level
+        ApplyLevelModifiers();
         base.Start();
         enemy = GetComponent<Enemy>();
+
+    }
+
+    private void ApplyLevelModifiers()
+    {
+        //Major
+        Modify(strength);
+        Modify(agility);
+        Modify(intelligence);
+        Modify(vitality);
+        //Attack
+        Modify(damage);
+        Modify(critChance);
+        Modify(critPower);
+        //Defense
+        Modify(maxHealth);
+        Modify(armor);
+        Modify(evasion);
+        Modify(magicalResistance);
+        //Elemental
+        Modify(fireDamage);
+        Modify(iceDamage);
+        Modify(metalDamage);
     }
 
     public override void TakeDamage(int _dmg)
     {
         base.TakeDamage(_dmg);
         enemy.DamageInpact();
+    }
+
+    protected void Modify(Stat _stat)
+    {
+        for (int i = 1; i < level; i++)
+        {
+            float modifier = _stat.GetValue() * percentageModifier;
+            _stat.AddModifiers(Mathf.RoundToInt(modifier));
+        }
     }
 }
